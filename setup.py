e23fe0e2e0d4cd3ddc01e4292a3d757963979472
Braden MacDonald <braden@opencraft.com>
2015-03-02 14:51:11 -0800

    Fix static data missing if not installed in dev mode

--- setup.py
+++ setup.py
@@ -22,6 +22,17 @@ import os
 import os.path
 from setuptools import setup
 
+
+def package_data(pkg, root_list):
+    """Generic function to find package_data for `pkg` under `root`."""
+    data = []
+    for root in root_list:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
 setup(
     name='xblock-utils',
     version='0.1a0',
@@ -31,5 +42,6 @@ setup(
     ],
     install_requires=[
         'XBlock',
-    ]
+    ],
+    package_data=package_data("xblockutils", ["public", "templates"]),
 )
